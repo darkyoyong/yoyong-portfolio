0769abfe43e1520dc2145fade2a1a9de34fef418
Jules Bryan Mapatac <jules.mapatac@gmail.com>
2021-08-14 11:57:04 +0800

    initial commit

---
+++ apps/yp-spotify/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'yoyongsportfolio-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'yp-spotify';
+}
